3e603c0c5a8469552f20f71319ed0030bd040518
Ernesta Petraityte <ernestaemilijapetraityte@gmail.com>
2020-03-26 14:34:59 +0200

    webpack prod env?

--- dqmgui/webpack.build.js
+++ dqmgui/webpack.build.js
@@ -1,14 +1,14 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-            REALM: JSON.stringify(process.env.REALM),
-            DQMGUI_URL: JSON.stringify(process.env.DQMGUI_URL),
+            DQMGUI_URL: JSON.stringify(env.DQMGUI_URL),
         }),
     ],
 });
